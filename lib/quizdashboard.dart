14e20ed64ca8ba6a610bf9a571e5f4596c71d0cc
Avan Fabian Daniswara <113287159+AvanFabian@users.noreply.github.com>
2024-11-04 05:18:03 +0700

    feat: Levelling, Library, Settings, dan Quiz Activity file page

--- lib/quizdashboard.dart
+++ lib/quizdashboard.dart
@@ -56,71 +56,76 @@ class Quizdashboard extends StatelessWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
-                  children: List.generate(
-                      4,
-                      (index) => Padding(
-                            padding: const EdgeInsets.only(top: 16.0),
-                            child: Card(
-                              elevation: 4.0,
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Row(
+                  children: List.generate(4, (index) {
+                    // List of image paths
+                    final imagePaths = [
+                      'assets/q1.png',
+                      'assets/q2.png',
+                      'assets/q3.png',
+                      'assets/q4.png',
+                    ];
+
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: Card(
+                        elevation: 4.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            children: <Widget>[
+                              // SizedBox inside Card
+                              SizedBox(
+                                width: 50.0,
+                                height: 50.0,
+                                child: Image.asset(
+                                  imagePaths[index], // Use different image for each card
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              // Rest of the card content
+                              const SizedBox(width: 32.0), // Horizontal gap
+                              const Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
-                                    // SizedBox inside Card
-                                    SizedBox(
-                                      width: 50.0,
-                                      height: 50.0,
-                                      child: Image.asset(
-                                        'assets/q1.png', // Replace with your local image path
-                                        fit: BoxFit.cover,
-                                      ),
+                                    Text(
+                                      'Tebak Gambar',
+                                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                                     ),
-                                    // const Spacer(), // Horizontal gap
-                                    // Column with 2 Text widgets
-                                    const SizedBox(width: 32.0), // Horizontal gap
-                                    const Expanded(
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Text(
-                                            'Tebak Gambar',
-                                            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                                          ),
-                                          SizedBox(height: 10.0), // Vertical gap between texts
-                                          Text(
-                                            'Tentukan Jawaban Sesuai Gambar',
-                                            style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w300),
-                                          ),
-                                        ],
-                                      ),
+                                    SizedBox(height: 10.0), // Vertical gap between texts
+                                    Text(
+                                      'Tentukan Jawaban Sesuai Gambar',
+                                      style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w300),
                                     ),
-                                    // Horizontal gap
-                                    const SizedBox(width: 16.0), // Horizontal gap
-                                    // Another SizedBox inside Card
-                                    const SizedBox(
-                                      width: 72.0,
-                                      height: 72.0,
-                                      child: Stack(
-                                        alignment: Alignment.center, // Center all children within the Stack
-                                        children: <Widget>[
-                                          CircularProgressIndicator(
-                                            value: 0.7, // Example progress value
-                                            strokeWidth: 5.0,
-                                          ),
-                                          Center(
-                                            child: Text(
-                                              '70%', // Example percentage text
-                                              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
-                                            ),
-                                          ),
-                                        ],
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(width: 16.0), // Horizontal gap
+                              const SizedBox(
+                                width: 72.0,
+                                height: 72.0,
+                                child: Stack(
+                                  alignment: Alignment.center, // Center all children within the Stack
+                                  children: <Widget>[
+                                    CircularProgressIndicator(
+                                      value: 0.7, // Example progress value
+                                      strokeWidth: 5.0,
+                                    ),
+                                    Center(
+                                      child: Text(
+                                        '70%', // Example percentage text
+                                        style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
                                       ),
                                     ),
                                   ],
                                 ),
                               ),
-                            ),
-                          )),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
                 ),
               ),
             ),
